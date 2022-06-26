984c4b00eb9e9da05ccc4f9778296398b1fa110f
vali8222 <valivali10298@gmail.com>
2022-06-26 17:24:15 +0300

    Init commit

---
+++ src/routers/user.js
@@ -0,0 +1,141 @@
+const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
+const router = new express.Router()
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+
+// ---------------> Create a user
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try{
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    
+
+})
+
+// ---------------> Log in a user
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+
+    } catch (e) {
+         res.status(400).send()
+    }
+})
+
+// ---------------> Log out a user
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+// ---------------> Log out all users
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }   
+})
+
+// ---------------> Read logged user
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+// ---------------> Update one user
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid update!'})
+    }
+
+    try {
+        const user = req.user;
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// ---------------> Delete one user
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.send(500).send(e)
+    }
+})
+
+// ---------------> Upload user avatar
+const avatar = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error('File must be an Image'))
+        }
+        
+        cb(undefined, true);
+    }
+})
+router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+// ---------------> Delete user avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+// ---------------> Get the avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+        
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+ 
+module.exports = router
